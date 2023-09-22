85332a3d367b84651c40c391472ca91fff193a1e
Rober Fabian Barreto <139162533+robersks@users.noreply.github.com>
2023-09-22 07:55:29 -0500

    feat: :sparkles: lectura de API

---
+++ index.js
@@ -0,0 +1,24 @@
+// referencia a tbody de la tabla
+const tablaBody = document.getElementById('tablaBody');
+
+// URL del API
+const url = 'https://650cf20447af3fd22f681060.mockapi.io/registro/registros';
+
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        // Recorre los datos y agrega cada registro a la tabla
+        data.forEach(registro => {
+            const { id, valor, tipo } = registro; // Extraer los valores de cada registro
+            const fila = `
+                <tr>
+                    <td>${id}</td>
+                    <td>${valor}</td>
+                    <td>${tipo}</td>
+                </tr>
+            `;
+            tablaBody.innerHTML += fila; // Agrega la fila a la tabla
+        });
+    })
+    .catch(err => console.error(err));
